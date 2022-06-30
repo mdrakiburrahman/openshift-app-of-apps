3150ae5e388d7b1171429503a9e63c1bd83b61ba
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2022-06-30 14:21:17 -0700

    Added ADC health check

--- arc-healthcheck/kustomize/base/configs/activedirectoryconnector.lua
+++ arc-healthcheck/kustomize/base/configs/activedirectoryconnector.lua
@@ -2,22 +2,22 @@ hs = {}
 
 if obj.status ~= nil then
   if obj.status.state == "Ready" then
-    hs.message = "SqlManagedInstance deployment finished"
+    hs.message = "ActiveDirectoryConnector deployment finished"
     hs.status = "Healthy"
     return hs
   end
   if obj.status.state == "Failed" then
-    hs.message = "Failed to deploy SqlManagedInstance"
+    hs.message = "Failed to deploy ActiveDirectoryConnector"
     hs.status = "Degraded"
     return hs
   end
   if obj.status.state == "Error" then
-    hs.message = "Failed to deploy SqlManagedInstance"
+    hs.message = "Failed to deploy ActiveDirectoryConnector"
     hs.status = "Degraded"
     return hs
   end
 end
 
 hs.status = "Progressing"
-hs.message = "SqlManagedInstance deployment in progress"
+hs.message = "ActiveDirectoryConnector deployment in progress"
 return hs
